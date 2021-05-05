de45fe66064398e962e9df6fb2672206654d03dd
Dosugamea <17107514+Dosugamea@users.noreply.github.com>
2021-05-05 14:40:29 +0900

    🍱 Update theme color

--- nuxt.config.js
+++ nuxt.config.js
@@ -65,13 +65,13 @@ export default {
       dark: true,
       themes: {
         dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
+          primary: colors.purple.base,
+          secondary: '#673AB7',
+          accent: colors.indigo.base,
+          error: colors.pink.base,
           warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3
+          info: colors.teal.base,
+          success: colors.green.base
         }
       }
     }
