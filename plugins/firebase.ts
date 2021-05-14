5f8e555a2246dd237345417a93b953d15f72307e
Dosugamea <17107514+Dosugamea@users.noreply.github.com>
2021-05-14 23:23:45 +0900

    ➕ Add firebase storage, and database dep

--- plugins/firebase.ts
+++ plugins/firebase.ts
@@ -1,6 +1,7 @@
 import firebase from 'firebase/app'
 import 'firebase/auth'
-import 'firebase/firestore'
+import 'firebase/database'
+import 'firebase/storage'
 
 // SDKを使用するためのConfig情報
 const firebaseConfig = {
@@ -12,6 +13,7 @@ const firebaseConfig = {
   appId: '1:492081702898:web:7c8200d69a8e019280ad6b',
   measurementId: 'G-95NZR8K3P8'
 }
+
 // Appの初期化
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig)
@@ -19,4 +21,5 @@ if (!firebase.apps.length) {
 
 export const auth = firebase.auth()
 export const google = new firebase.auth.GoogleAuthProvider()
-export const firestore = firebase.firestore()
+export const database = firebase.database()
+export const storage = firebase.storage()
