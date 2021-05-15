f7adfda4c7ab7f624a06cf17f726fec5a73c19ac
Dosugamea <17107514+Dosugamea@users.noreply.github.com>
2021-05-16 06:15:52 +0900

    🍱 Add background color

--- layouts/default.vue
+++ layouts/default.vue
@@ -8,7 +8,7 @@
       :open-drawer.sync="drawer"
       :right-sided.sync="right"
     />
-    <v-main>
+    <v-main class="has-yellow">
       <v-container>
         <nuxt />
       </v-container>
@@ -35,3 +35,38 @@ export default class Default extends Vue {
   drawer: boolean = false
 }
 </script>
+
+<style>
+  .theme--light.v-application {
+    color: #FFF ! important;
+  }
+  .has-bg {
+    background-size: cover;
+    background-image: url("~@/assets/images/phil-hearing-jd2-K2aBwUY-unsplash.jpg");
+  }
+  .has-bg::before{
+    /* 透過した黒を上から重ねるイメージ */
+    background-color: rgba(0,0,0,0.1);
+    /* 自由に位置指定 */
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    content: ' ';
+  }
+  .has-yellow {
+    background-color: #ffff99;
+  }
+  .has-yellow::before{
+    /* 透過した黒を上から重ねるイメージ */
+    background-color: rgba(0,0,0,0.38);
+    /* 自由に位置指定 */
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    content: ' ';
+  }
+</style>
