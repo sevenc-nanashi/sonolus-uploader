1ce6cd3a4541b6908b421f91744384fd6d44c0a7
Dosugamea <17107514+Dosugamea@users.noreply.github.com>
2021-05-16 02:43:31 +0900

    ➕ Add @nuxtjs/markdownit

--- nuxt.config.js
+++ nuxt.config.js
@@ -45,8 +45,13 @@ export default {
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
     // https://go.nuxtjs.dev/pwa
-    '@nuxtjs/pwa'
+    '@nuxtjs/pwa',
+    '@nuxtjs/markdownit'
   ],
+
+  markdownit: {
+    runtime: true // Support `$md()`
+  },
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {},
