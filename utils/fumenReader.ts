e1d2ee61932de6f72a857a5f380305076286fa32
Dosugamea <17107514+Dosugamea@users.noreply.github.com>
2021-06-08 22:26:30 +0900

    🚧 Change fumenReader to potato based

--- utils/fumenReader.ts
+++ utils/fumenReader.ts
@@ -1,16 +1,40 @@
-import { Fumen as FumenType } from '@/types/upload/fumen'
-import { $axios } from '@/utils/api'
-import { SortKey, SortOrder, FilterGenreKey, FilterDifficultyKey } from '../types/fumenReader'
+import { LevelsApi, GetLevelListResponse, UsersApi } from '@/potato'
+import { SortKey, SortOrder, FilterGenreKey, FilterDifficultyKey } from '../types/search'
 
-interface GetLevelListResponse {
-  pageCount: number
-  levels: FumenType[]
+async function getLevelList (
+  $levelsApi: LevelsApi,
+  page: number,
+  keyword: string | undefined,
+  sort: SortKey | undefined,
+  order: SortOrder | undefined,
+  genre: FilterGenreKey | undefined,
+  difficulty: FilterDifficultyKey | undefined
+): Promise<GetLevelListResponse> {
+  let keywords: string = keyword !== undefined ? `${keyword}` : ''
+  keywords += sort !== undefined ? ` sort:${sort}` : ''
+  keywords += order !== undefined ? ` order:${order}` : ''
+  keywords += genre !== undefined ? ` genre:${genre}` : ''
+  keywords += difficulty !== undefined ? ` difficulty:${difficulty[0]}-${difficulty[1]}` : ''
+  const resp = await $levelsApi.getLevelList(undefined, page - 1, keywords)
+  return resp.data
 }
 
-async function getLevelList (keyword: string, page: number, sort: SortKey, order: SortOrder, genre: FilterGenreKey, difficulty: FilterDifficultyKey) : Promise<GetLevelListResponse> {
-  page -= 1
-  const keywords = `${keyword} sort:${sort} order:${order} genre:${genre} difficulty:${difficulty[0]}-${difficulty[1]}`
-  const resp = await $axios.get('http://localhost:8080/levels/list', { params: { page, keywords } })
-  return { pageCount: resp.data.pageCount, levels: resp.data.items as FumenType[] } as GetLevelListResponse
+async function getUserLevelList (
+  $usersApi: UsersApi,
+  userId: string,
+  page: number,
+  keyword: string | undefined,
+  sort: SortKey | undefined,
+  order: SortOrder | undefined,
+  genre: FilterGenreKey | undefined,
+  difficulty: FilterDifficultyKey | undefined
+): Promise<GetLevelListResponse> {
+  let keywords: string = keyword !== undefined ? `${keyword}` : ''
+  keywords += sort !== undefined ? ` sort:${sort}` : ''
+  keywords += order !== undefined ? ` order:${order}` : ''
+  keywords += genre !== undefined ? ` genre:${genre}` : ''
+  keywords += difficulty !== undefined ? ` difficulty:${difficulty[0]}-${difficulty[1]}` : ''
+  const resp = await $usersApi.getUsersLevels(userId, undefined, page - 1, keywords)
+  return resp.data
 }
-export { getLevelList }
+export { getLevelList, getUserLevelList }
