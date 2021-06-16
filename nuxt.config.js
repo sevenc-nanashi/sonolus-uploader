ec3889839424da8faec3449bd43979ed5fe6fdfc
Dosugamea <17107514+Dosugamea@users.noreply.github.com>
2021-06-17 00:04:49 +0900

    🎨 Enable extractCSS option

--- nuxt.config.js
+++ nuxt.config.js
@@ -191,6 +191,7 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    extractCSS: true,
     babel: {
       plugins: [['@babel/plugin-proposal-private-methods', { loose: true }]]
     },
